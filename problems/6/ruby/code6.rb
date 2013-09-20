47475529b640085ba9e28e6dc690fa43eac8bc70
Minh Ha <nhat.minh.ha@gmail.com>
2013-09-20 15:37:12 +1200

    finished ruby, brute force of prob 6

---
+++ problems/6/ruby/code6.rb
@@ -0,0 +1,46 @@
+# The sum of the squares of the first ten natural numbers is,
+# 12 + 22 + ... + 102 = 385
+
+# The square of the sum of the first ten natural numbers is,
+# (1 + 2 + ... + 10)2 = 552 = 3025
+
+# Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+# BRUTE FORCE
+
+def square_of_number(num)
+  num ** 2
+end
+
+def calculate_sum(numbers)
+  numbers.inject { |sum, n| sum + n }
+end
+
+def build_numbers(low, high)
+  @squares_of_numbers = []
+  @numbers = []
+  (low..high).each do |number|
+    @squares_of_numbers << square_of_number(number)
+    @numbers << number
+  end
+end
+
+def sum_square_difference(low = 1, high = 100)
+  build_numbers(low, high)
+  
+  sum_square_of_numbers = calculate_sum(@squares_of_numbers)
+  sum_of_numbers = calculate_sum(@numbers)
+  square_of_sum_of_numbers = square_of_number(sum_of_numbers)
+  
+  square_of_sum_of_numbers - sum_square_of_numbers
+end
+
+p sum_square_difference()
+
+# TODO
+# spec for the optimised version
+# limit = 100
+# sum = limit(limit + 1)/2
+# sum_of_sq = (2limit + 1)(limit + 1)limit/6
+# p sum^2 − sum_of_sq
