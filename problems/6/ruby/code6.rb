8475c7c91efe7b2da1867febcff80dcc4b0070c8
Minh Ha <nhat.minh.ha@gmail.com>
2013-09-20 15:54:10 +1200

    barebone script version

--- problems/6/ruby/code6.rb
+++ problems/6/ruby/code6.rb
@@ -9,10 +9,6 @@
 
 # BRUTE FORCE
 
-def square_of_number(num)
-  num ** 2
-end
-
 def calculate_sum(numbers)
   numbers.inject { |sum, n| sum + n }
 end
@@ -21,7 +17,7 @@ def build_numbers(low, high)
   @squares_of_numbers = []
   @numbers = []
   (low..high).each do |number|
-    @squares_of_numbers << square_of_number(number)
+    @squares_of_numbers << number ** 2
     @numbers << number
   end
 end
@@ -31,12 +27,22 @@ def sum_square_difference(low = 1, high = 100)
   
   sum_square_of_numbers = calculate_sum(@squares_of_numbers)
   sum_of_numbers = calculate_sum(@numbers)
-  square_of_sum_of_numbers = square_of_number(sum_of_numbers)
+  square_of_sum_of_numbers = sum_of_numbers ** 2
   
   square_of_sum_of_numbers - sum_square_of_numbers
 end
 
-p sum_square_difference()
+p sum_square_difference
+
+# BAREBONE Script Version
+sum = 0
+sum_square = 0
+(1..100).each do |num|
+  sum = sum + num
+  sum_square = sum_square + num ** 2
+end
+p (sum ** 2) - sum_square
+
 
 # TODO
 # spec for the optimised version
