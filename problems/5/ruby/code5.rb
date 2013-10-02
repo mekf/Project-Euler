178d8aa3419ede529122e45d44c5e820a405654a
Minh Ha <nhat.minh.ha@gmail.com>
2013-10-03 03:24:00 +1300

    ruby version of problem 5

--- problems/5/ruby/code5.rb
+++ problems/5/ruby/code5.rb
@@ -4,16 +4,21 @@
 # i = 2520
 # condition
 # while condition false
-# i += 1
+# i += 2520
 
-@i = 1
+# 2520 is given as divisible to (1..10)
+# therefore we can check increments of 2520 to see if there is a number divisible to (11..20)
+
+
+@i = 2520
+@divisible_numbers = Array(11..20)
 
 def evenly_divisible
-  divisible_results = Array(2..20).map { |num| @i % num == 0 }
+  divisible_results = @divisible_numbers.map { |num| @i % num == 0 }
   !divisible_results.include?(false)
 end
 
 while evenly_divisible == false
-  @i += 1
+  @i += 2520
   p @i
 end
